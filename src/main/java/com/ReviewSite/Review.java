362f900b093161e9dc2bf5ce3aff4f38f44853b2
JSnyder <jesse.c.snyder@gmail.com>
2017-10-27 14:28:38 -0400

    first push

---
+++ src/main/java/com/ReviewSite/Review.java
@@ -0,0 +1,47 @@
+package com.ReviewSite;
+
+public class Review {
+	
+	protected long id;
+	private String title;
+	private String image;
+	private String category;
+	private String content;
+	
+	public Review(long id, String title, String image, String category, String content) {
+		this.id = id;
+		this.title = title;
+		this.image = image;
+		this.category = category;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	@Override
+	public String toString() {
+		return "\nid: \t " + id + "\nTitle: \t" + title + "\nImage: \t" + image + "\nCategory: \t"
+				+ category + "\nContent: \t" + content + "\n";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/ReviewSite/ReviewController.java
@@ -0,0 +1,30 @@
+package com.ReviewSite;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ReviewController {
+	
+	@Resource
+	ReviewRepository repository = new ReviewRepository();
+	
+	
+    @RequestMapping("/reviews")
+    public String getAllReviews(Model model) {
+        model.addAttribute("reviews", repository.findAll());
+        return "Reviews";
+    }
+    
+    @RequestMapping("/review")
+    public String findOne(@RequestParam(value = "id") Long id, Model model) {
+    	model.addAttribute("reviews", repository.findOne(id));
+    	return "Review";
+    	
+    }
+
+}
